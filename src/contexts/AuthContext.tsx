374e9ee007b684f253413eebff49c865e9165a07
joeyanovich <joedison.dias@gmail.com>
2022-11-03 20:21:09 -0300

    :hammer:Add: created context for user login

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, ReactNode } from "react";
+
+interface UserProps {
+    name: string;
+    avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+    user: UserProps;
+    signIn: () => Promise<void>
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps){
+    async function signIn() {
+        console.log('Vamos logar!');
+    }
+    return (
+        <AuthContext.Provider value={{
+            signIn,
+            user: {
+                name: 'Joedison',
+                avatarUrl: 'http://github.com/joeyanovich.png'
+            }
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
